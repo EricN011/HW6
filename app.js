e5e7224234b0334bcf606c55ea7077483a35f4ba
Eric Noll <eanoll88@gmail.com>
2019-10-15 18:32:14 -0500

    weather update

---
+++ app.js
@@ -0,0 +1,20 @@
+// database
+// raw weather object from API
+
+// variables
+// parsed weather data
+
+// utility functions
+// get raw data
+// parse raw data
+// render parsed data
+
+// event functions
+// search button click
+// have city name
+// send city name to openweather API
+// set weather info to the object return (see raw data)
+
+// init
+
+// check local storage for city history
