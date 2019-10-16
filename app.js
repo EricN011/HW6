a7ef7627f38ca675fbf14ab9b93e352d9b607d85
Eric Noll <eanoll88@gmail.com>
2019-10-15 21:14:40 -0500

    weather update

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-// database
-// raw weather object from API
+$(document).ready(function() {
+  // database
+  // raw weather object from API
+  var cityWeather =
+    "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22";
 
-// variables
-// parsed weather data
+  // variables
+  // parsed weather data
 
-// utility functions
-// get raw data
-// parse raw data
-// render parsed data
+  // utility functions
+  // get raw data
+  // parse raw data
+  // render parsed data
 
-// event functions
-// search button click
-// have city name
-// send city name to openweather API
-// set weather info to the object return (see raw data)
+  // event functions
+  // search button click
+  // have city name
+  // send city name to openweather API
+  // set weather info to the object return (see raw data)
 
-// init
+  // init
 
-// check local storage for city history
+  // check local storage for city history
+  $.ajax({
+    url: cityWeather,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+});
